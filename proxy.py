e7f3cfadcaed0b6b952ea3b87ff634636fa8ec1c
Sergia <sergia94@protonmail.com>
2023-01-20 21:15:44 +0100

    Disable websockets

--- proxy.py
+++ proxy.py
@@ -1,11 +1,26 @@
 from base_reverse_proxy import BaseReverseProxyHandler
 from http.server import ThreadingHTTPServer
+from threading import Lock
 
 # configuration
 destination_schema = 'https'
 destination_host = 'dair-community.social'
 listen_port = 8083
 
+class MastodonEthicalProxy(BaseReverseProxyHandler):
+    """Ethical proxy."""
+    
+    def disable_websockets(self, response):
+        """Disable websockets to have no push notifications (only pull)."""
+        if 'text/html' not in response.headers.get('content-type', ''):
+            return
+        to_replace = ('wss://' + self.get_destination_host()).encode('utf-8')
+        response._content = response.content.replace(to_replace, b'wss://0.0.0.0')
+    
+    def process_response(self, response):
+        """Process the response from the destination."""
+        super(MastodonEthicalProxy, self).process_response(response)
+        self.disable_websockets(response)
 
 def run():
     """Run the server."""
