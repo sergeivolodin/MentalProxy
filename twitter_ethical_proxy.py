23e52d8b3d6cdaea99b64d80fae96ace51947a22
Sergia <sergia94@protonmail.com>
2023-01-25 14:43:15 +0100

    add basic Twitter code

---
+++ twitter_ethical_proxy.py
@@ -0,0 +1,53 @@
+from .base_reverse_proxy import BaseReverseProxyHandler
+from .http_tools import HTTPTools
+
+
+class BaseTwitterEthicalProxy(BaseReverseProxyHandler, HTTPTools):
+    """Ethical proxy."""
+    
+    @property
+    def rate_limiter(self):
+        raise NotImplemented('Please use with_limit()')
+    
+    @property
+    def destination_schema(self):
+        raise 'https'
+    
+    @property
+    def destination_host(self):
+        raise 'mobile.twitter.com'
+    
+    @classmethod
+    def with_limit(cls, rlim):
+        """Add a rate limiter for all threads."""
+        if rlim is None:
+            raise ValueError("Please supply a ratelimiter")
+        
+        class TwitterEthicalProxy(cls):
+            @property
+            def rate_limiter(self):
+                return rlim
+        
+        return TwitterEthicalProxy
+    
+    def filter_incoming_request(self):
+        pass
+        # if 'api/v1/notifications' in self.path and not self.rate_limiter.notifications_request_ok():
+        #     self.send_empty_json_array()
+
+        #     # self.send_error(403, "Notifications muted for mental wellbeing")
+        #     return True
+            
+        # if '/api/v1/timelines/' in self.path and not self.rate_limiter.timeline_request_ok():
+            
+        #     # send a 403 with a message
+        #     self.send_error(403, f"Timeline paused for {int(self.rate_limiter.notifications_remaining_time)} more seconds...")
+            
+        #     # send an empty json (no error, shows as empty)
+        #     # self.send_empty_json()
+        #     return True
+    
+    def process_response(self, response):
+        """Process the response from the destination."""
+        super(BaseTwitterEthicalProxy, self).process_response(response)
+        self.disable_websockets(response)
---
+++ twitter_proxy.py
@@ -0,0 +1,22 @@
+from http.server import ThreadingHTTPServer
+from argparse import ArgumentParser
+from rate_limiter import RateLimiterPostsNotifications, RateLimiterAlwaysNo, RateLimiterAlwaysYes, RateLimiterPostsPerMinute
+from twitter_ethical_proxy import BaseTwitterEthicalProxy
+
+# argument parser
+parser = ArgumentParser(description="Run a proxy for Twitter with mental wellbeing features")
+parser.add_argument('--listen_port', type=str, help="Port to listen on, default is 8080", default=8080)
+
+def run(listen_port):
+    """Run the server."""
+    server_address = ('', listen_port)
+    ratelimiter = RateLimiterPostsNotifications()
+    handler_class = BaseTwitterEthicalProxy\
+        .with_limit(ratelimiter)
+    httpd = ThreadingHTTPServer(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(f"Now open http://127.0.0.1:{args.listen_port} to access Twitter with mental wellbeing features")
+    run(int(args.listen_port), args.instance)
